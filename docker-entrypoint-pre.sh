9da846c3d8d464a5abde9869b63cb7e6f9bf55b4
yosefy <yosef.yudilevich@gmail.com>
2018-04-11 04:37:48 +0300

    chenged docker-entrypoint-pre.sh to work with new file

--- docker-entrypoint-pre.sh
+++ docker-entrypoint-pre.sh
@@ -2,8 +2,7 @@
 
 # Perfom all the needed preprocessing here...
 cat spec/daily_lesson_spec.js
-sed -i "s#https://archive.kbb1.com#$TEST_ADDR#g" spec/daily_lesson_spec.js
-sed -i "s#headless: false#$BROWSER_FLAGS#g" spec/daily_lesson_spec.js
+sed -i "s#https://archive.kbb1.com#$TEST_ADDR#g" spec/testconfig.json
 
 # Invoke the original entrypoint passing the command and arguments
 exec  $@ 
