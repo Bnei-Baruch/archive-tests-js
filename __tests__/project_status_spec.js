37faad10e2dc1d39d78720b80af8a8afafbb110a
Benyamin Rafaeli <benyamin.r@fintier.com>
2018-05-16 05:35:57 +0300

    Updated beforeall setup

---
+++ __tests__/project_status_spec.js
@@ -0,0 +1,45 @@
+const puppeteer = require('puppeteer');
+const testconfig = require(__dirname + '/testconfig.json');
+const width = 1920;
+const height = 1080;
+let browser;
+let page;
+let originalTimeout;
+
+
+beforeAll((async function () {
+    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+    browser = await puppeteer.launch(testconfig.browser);
+    page = await browser.newPage();
+    await page.setViewport({width, height});
+}));
+
+describe('Project Status Page Test Suite => ', function () {
+
+    it('Header - Displayed', async function () {
+        await page.goto(testconfig.resources.projectStatusUrl, {waitUntil: 'networkidle2'});
+        // header
+        expect(await page.$('.aligned.header')).toBeDefined();
+        // header title
+        expect(await page.$eval('.aligned.header', (selector) => {
+            return selector.innerHTML
+        })).toBe('What\'s new?');
+
+        let filters = await page.$$eval('.ui.header', (selectors) => {
+            return selectors.map(selector => selector.innerText)
+        });
+        expect(filters.length).toBe(6);
+        // expect(filters[0].trim()).toEqual('Baal HaSulam');
+        // expect(filters[1].trim()).toEqual('Rabash');
+        // expect(filters[2].trim()).toEqual('Michael Laitman');
+
+    });
+
+});
+
+
+afterAll(async function () {
+    await browser.close();
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+});
