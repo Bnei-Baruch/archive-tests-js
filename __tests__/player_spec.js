38ff9ca89135eedae8b7f631d34eb820ab40cbba
Benyamin Rafaeli <benyamin.r@fintier.com>
2018-05-19 10:28:33 +0300

    last commit for unit mat

--- __tests__/player_spec.js
+++ __tests__/player_spec.js
@@ -39,14 +39,14 @@ describe('Player Test Suite => ', function () {
     });
 
     it("speedSelector_1X", async function () {
-        // // playerUrl
+        // playerUrl
         // await page.goto(testconfig.resources.playerUrl, {waitUntil: 'networkidle2'});
         //
         // let playerButtons = await page.$$eval('.mediaplayer__controls button', (selectors) => {
         //     return selectors.map(selector => selector.innerHTML)
         // });
         // expect(playerButtons.length).toBe(10);
-        //
+
         // await page.click('.mediaplayer__controls > div.buttons-wrapper > button');
         //
         //
