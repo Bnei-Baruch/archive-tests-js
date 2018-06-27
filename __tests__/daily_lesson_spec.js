3ca37eb537807155f4eca0086051d1b0a7f65c55
Benyamin Rafaeli <benyamin.r@fintier.com>
2018-06-27 05:07:26 +0300

    Refactor for main test class

--- __tests__/daily_lesson_spec.js
+++ __tests__/daily_lesson_spec.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+¡const puppeteer = require('puppeteer');
 const utils = require('../utils.js');
 const testconfig = require('./testconfig');
 const width = 1490;
--- __tests__/main_spec.js
+++ __tests__/main_spec.js
@@ -20,45 +20,54 @@ describe('Main Page Test Suite => ', function () {
 
     it('Header - Displayed', async function () {
         await page.goto(testconfig.resources.mainUrl, {waitUntil: 'networkidle2'});
-        // header
+        // is header defined
         expect(await page.$('.homepage__title')).toBeDefined();
 
-        // header title
-        expect(await page.$eval('.homepage__title', (selector) => {
-            return selector.innerHTML
-        })).toBe('Explore the wisdom of Kabbalah');
+        // check header title
+        expect(await page.$eval('.homepage__title', selector => selector.innerText))
+            .toBe('Explore the wisdom of Kabbalah');
+        // check search button text
+        expect(await page.$eval('.search-omnibox button', selector => selector.innerText))
+            .toBe('search');
+        // check homepage__featured should be displayed 2 items
+        expect(await page.$$eval('.thumbnail', selectors => selectors.length))
+            .toBeCloseTo(2);
 
-        expect(await page.$eval('.search-omnibox button', (selector) => {
-            return selector.innerHTML
-        })).toBe('search');
 
-        let filters = await page.$$eval('.thumbnail', (selectors) => {
-            return selectors.map(selector => selector.text)
-        });
-        expect(filters.length).toBe(2);
+        // let filters = await page.$$eval('.horizontal.divider', (selectors) => {
+        //     return selectors.map(selector => selector.innerText)
+        // });
+        // expect(filters.length).toBe(2);
+        // expect(filters[0].trim()).toEqual('ARCHIVE SECTIONS');
+        // expect(filters[1].trim()).toEqual('LATEST UPDATES');
 
-        filters = await page.$$eval('.horizontal.divider', (selectors) => {
-            return selectors.map(selector => selector.innerText)
-        });
-        expect(filters.length).toBe(2);
-        expect(filters[0].trim()).toEqual('ARCHIVE SECTIONS');
-        expect(filters[1].trim()).toEqual('LATEST UPDATES');
+        // .homepage__website-sections .homepage__iconsrow .row
 
-        filters = await page.$$eval('.sidebar-item', (selectors) => {
-        // filters = await page.$$eval('a.small.header', (selectors) => {
+        expect(await page.$$eval('.horizontal.divider', selector => selector.length))
+            .toBeCloseTo(2);
+
+        expect(await page.$$eval('.homepage__website-sections .header',
+            (selectors) => {
+                return selectors.map(selector => selector.innerText.trim())
+            })).toEqual(['Lessons & Lectures', 'Programs', 'Library', 'Conventions & Events', 'Publications']);
+
+
+        // .toEqual(['ARCHIVE SECTIONS', 'LATEST UPDATES']);
+
+        // expect(filters.length).toBe(2);
+        // expect(filters[0].trim()).toEqual('ARCHIVE SECTIONS');
+        // expect(filters[1].trim()).toEqual('LATEST UPDATES');
+
+
+        let filters = await page.$$eval('.sidebar-item', (selectors) => {
             return selectors.map(selector => selector.innerText)
         });
         expect(filters.length).toBe(8);
-        // expect(filters[0].trim()).toEqual('Daily Kabbalah Lesson');
         expect(filters[0].trim()).toEqual('Lessons & Lectures');
         expect(filters[1].trim()).toEqual('Programs');
-        // expect(filters[2].trim()).toEqual('Lectures & Lessons');
         expect(filters[2].trim()).toEqual('Library');
-        // expect(filters[3].trim()).toEqual('Library');
         expect(filters[3].trim()).toEqual('Conventions & Events');
-        // expect(filters[4].trim()).toEqual('Conventions & Events');
         expect(filters[4].trim()).toEqual('Topics');
-        // expect(filters[5].trim()).toEqual('Publications');
         expect(filters[5].trim()).toEqual('Publications');
         expect(filters[6].trim()).toEqual('Project Status');
         expect(filters[7].trim()).toEqual('Old Site');
