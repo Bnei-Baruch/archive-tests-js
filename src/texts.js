ed4be4a8c82a0260916c62043395fab1b50b25ea
Alona Fraiman <alona@bbdomain>
2018-07-11 17:27:51 +0300

    add All All Elements Exists for publications

--- src/texts.js
+++ src/texts.js
@@ -31,10 +31,21 @@ const texts = {
         logo: 'Kabbalah Media',
         donateButton: 'DONATE NOW',
         languageDropDown: 'English',
-        sideBar: ['Lessons & Lectures', 'Programs', 'Library', 'Conventions & Events', 'Topics', 'Publications', 'Project Status', 'Old Site'],
+        sideBar: ['Lessons & Lectures', 'Programs', 'Library', 'Conventions & Events', 'Topics', 'Publications', 'Project Status', 'Twitter', 'Old Site'],
         footer: 'Kabbalah MediaCopyright © 2003-2018 Bnei Baruch – Kabbalah L’Am Association, All rights reserved',
         title: 'Programs',
         header: 'Here you will find a rich variety of programs that include: fascinating conversations about the meaning of life, current events, programs for the whole family and more.',
+        filterTabs: ['Genre/Program','Topics','Sources','Date'],
+    },
+    publications:{
+        logo: 'Kabbalah Media',
+        donateButton: 'DONATE NOW',
+        languageDropDown: 'English',
+        sideBar: ['Lessons & Lectures', 'Programs', 'Library', 'Conventions & Events', 'Topics', 'Publications', 'Project Status', 'Twitter', 'Old Site'],
+        footer: 'Kabbalah MediaCopyright © 2003-2018 Bnei Baruch – Kabbalah L’Am Association, All rights reserved',
+        title: 'Publications',
+        header: 'Articles on current events, culture, popular science, relationships and more, presented from the unique perspective of the wisdom of Kabbalah',
+        filterTbl: ['Publishers','Date'],
         filterTabs: ['Genre/Program', 'Topics', 'Sources', 'Date'],
     }
 
