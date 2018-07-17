686fc9d2674c96e9630ac435c6865308bfa39535
yosefy <yosefy@users.noreply.github.com>
2018-07-17 04:30:44 +0300

    Update docker-entrypoint-pre.sh

--- docker-entrypoint-pre.sh
+++ docker-entrypoint-pre.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # Perfom all the needed preprocessing here...
-cat spec/daily_lesson_spec.js
-sed -i "s#https://archive.kbb1.com#$TEST_ADDR#g" spec/testconfig.json
-sed -i "s#\"headless\": false#$BROWSER_FLAGS#g" spec/testconfig.json
+
+cat __tests__/testconfig.json | \
+jq ".browser = `cat __tests__/docker_browser_flags.json`" > \
+tmp.json && mv tmp.json testconfig1.json
+
+
+sed -i "s#https://archive.kbb1.com#$TEST_ADDR#g" __tests__/testconfig.json
+sed -i "s#\"headless\": false#$BROWSER_FLAGS#g" __tests__/testconfig.json
 
 # Invoke the original entrypoint passing the command and arguments
 exec  $@ 
